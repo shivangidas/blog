a398e3c2c06cff80408ea2d2e510c6a95cd90aa5
shivangi <shivangi.das7@gmail.com>
2019-02-03 23:04:04 +0530

    just a post idea

--- src/posts/post1.js
+++ src/posts/post1.js
@@ -7,8 +7,12 @@ class Post extends Component {
     return (
       <div className="Post">
         <header className="Post-header">
-          <h1>I am pos 1</h1>
-          <Image alt="Books" src={books} height="300px" />
+          <h1>I am post 1</h1>
+          <Image alt="Books" src={books} height="200px" />
+          <p>
+            Oh God! Routing was so easy and I was so confused by how to do it.
+            The first post will be about routing when I get to writing it.
+          </p>
         </header>
       </div>
     );
