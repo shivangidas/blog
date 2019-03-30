a48fbc1b7c60336722c1bebf3f5de6cac16e6edc
shivangi <shivangi.das7@gmail.com>
2019-03-30 14:12:47 +0530

    Updates

---
+++ precache-manifest.a313b8aa7f3249b0a7244e826c889d23.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbf3350d737fd7b46901e47a38c6bc52",
+    "url": "/static/media/star_list.cbf3350d.svg"
+  },
+  {
+    "revision": "9a6c97a7865dd0d4d8f9a870b479c4fb",
+    "url": "/static/media/books8.9a6c97a7.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7a72a70435d551f8aaa6",
+    "url": "/static/js/main.7a72a704.chunk.js"
+  },
+  {
+    "revision": "007869cca28610468358",
+    "url": "/static/js/1.007869cc.chunk.js"
+  },
+  {
+    "revision": "7a72a70435d551f8aaa6",
+    "url": "/static/css/main.5af9758f.chunk.css"
+  },
+  {
+    "revision": "007869cca28610468358",
+    "url": "/static/css/1.7ce87094.chunk.css"
+  },
+  {
+    "revision": "82d733b10f6df55085a3b2f901eb3ecb",
+    "url": "/index.html"
+  }
+];
--- src/components/Bio.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-
-class Layout extends Component {
-  render() {
-    return <p>I like to code and read books and write about these things.</p>;
-  }
-}
-export default Layout;
