b155df7f3dbb217d2b45ec97c4760a3d63f728a7
shivangi <shivangi.das7@gmail.com>
2019-03-30 15:12:57 +0530

    test

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Layout extends Component {
   render() {
-    return <p>I like to code and read books and write about these things.</p>;
+    return <p>I like to code and read books and write about these things </p>;
   }
 }
 export default Layout;
