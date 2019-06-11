09c81e8cf037dda290fcc1c5932ce315eb203835
shivangi <shivangi@allgovision.com>
2019-06-11 17:00:41 +0530

    Updates

---
+++ precache-manifest.5dca26172beec459c9f184b139ef71ea.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0d72180028e051b6cefe8398ea071d6",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "58346889896a25a84ea2",
+    "url": "/blog/static/css/2.ca025b70.chunk.css"
+  },
+  {
+    "revision": "8a7849a4310df213c771",
+    "url": "/blog/static/css/main.f8a97a71.chunk.css"
+  },
+  {
+    "revision": "58346889896a25a84ea2",
+    "url": "/blog/static/js/2.a01d3f23.chunk.js"
+  },
+  {
+    "revision": "8a7849a4310df213c771",
+    "url": "/blog/static/js/main.56ab7975.chunk.js"
+  },
+  {
+    "revision": "eeecc51c3b4e3b0a67a2",
+    "url": "/blog/static/js/runtime~main.516a48d0.js"
+  },
+  {
+    "revision": "56733db5584b0e4e8e082495741a602d",
+    "url": "/blog/static/media/blogger.56733db5.PNG"
+  },
+  {
+    "revision": "0ffb4901c5402bc0a781deb946295e92",
+    "url": "/blog/static/media/icomoon.0ffb4901.svg"
+  },
+  {
+    "revision": "27486e857c102ecd168b9a54073325ad",
+    "url": "/blog/static/media/icomoon.27486e85.ttf"
+  },
+  {
+    "revision": "b685eba466302ea5642205c19a9e698c",
+    "url": "/blog/static/media/icomoon.b685eba4.woff"
+  },
+  {
+    "revision": "bba726b671c04eb61e2f7d5ced36c1a9",
+    "url": "/blog/static/media/icomoon.bba726b6.eot"
+  },
+  {
+    "revision": "cbf3350d737fd7b46901e47a38c6bc52",
+    "url": "/blog/static/media/star_list.cbf3350d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blog/precache-manifest.462120028cfad88d038f2a13a6b3fb2a.js"
+  "/blog/precache-manifest.5dca26172beec459c9f184b139ef71ea.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/blog/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blog/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
