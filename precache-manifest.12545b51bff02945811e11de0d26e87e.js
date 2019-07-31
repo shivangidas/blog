86e52b464e49e5192e7351cbecd2239fa4c7738d
shivangi <shivangi@allgovision.com>
2019-07-31 16:28:37 +0530

    Updates

---
+++ precache-manifest.12545b51bff02945811e11de0d26e87e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4fd837f09ca2a26e900bdfda5bbf312",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "a1b9230e68cf1cf563dd",
+    "url": "/blog/static/css/2.ca025b70.chunk.css"
+  },
+  {
+    "revision": "275a7ce529a3ca255408",
+    "url": "/blog/static/css/main.b465a838.chunk.css"
+  },
+  {
+    "revision": "a1b9230e68cf1cf563dd",
+    "url": "/blog/static/js/2.0607930d.chunk.js"
+  },
+  {
+    "revision": "275a7ce529a3ca255408",
+    "url": "/blog/static/js/main.426f20f5.chunk.js"
+  },
+  {
+    "revision": "eeecc51c3b4e3b0a67a2",
+    "url": "/blog/static/js/runtime~main.516a48d0.js"
+  },
+  {
+    "revision": "56733db5584b0e4e8e082495741a602d",
+    "url": "/blog/static/media/blogger.56733db5.PNG"
+  },
+  {
+    "revision": "0ffb4901c5402bc0a781deb946295e92",
+    "url": "/blog/static/media/icomoon.0ffb4901.svg"
+  },
+  {
+    "revision": "27486e857c102ecd168b9a54073325ad",
+    "url": "/blog/static/media/icomoon.27486e85.ttf"
+  },
+  {
+    "revision": "b685eba466302ea5642205c19a9e698c",
+    "url": "/blog/static/media/icomoon.b685eba4.woff"
+  },
+  {
+    "revision": "bba726b671c04eb61e2f7d5ced36c1a9",
+    "url": "/blog/static/media/icomoon.bba726b6.eot"
+  },
+  {
+    "revision": "cbf3350d737fd7b46901e47a38c6bc52",
+    "url": "/blog/static/media/star_list.cbf3350d.svg"
+  }
+]);
