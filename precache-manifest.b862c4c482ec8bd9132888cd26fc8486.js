9615c7d92af73d1eeeccbb62770415b7c0f22603
Shivangi Das <shivangi@Shivangis-MacBook-Pro.local>
2019-10-01 21:20:50 +0100

    Updates

---
+++ precache-manifest.b862c4c482ec8bd9132888cd26fc8486.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f22c54980a632a1eb64963700b487dd5",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "4c9ecf75f9e4454fd44a",
+    "url": "/blog/static/css/2.ca025b70.chunk.css"
+  },
+  {
+    "revision": "6730a84b9f99c98bbf4a",
+    "url": "/blog/static/css/main.103a0d5a.chunk.css"
+  },
+  {
+    "revision": "4c9ecf75f9e4454fd44a",
+    "url": "/blog/static/js/2.56d96fc5.chunk.js"
+  },
+  {
+    "revision": "6730a84b9f99c98bbf4a",
+    "url": "/blog/static/js/main.6b41ff3d.chunk.js"
+  },
+  {
+    "revision": "eeecc51c3b4e3b0a67a2",
+    "url": "/blog/static/js/runtime~main.516a48d0.js"
+  },
+  {
+    "revision": "77e54c15db377272ca0743e9a0eae3d5",
+    "url": "/blog/static/media/ChristmasSteps.77e54c15.jpg"
+  },
+  {
+    "revision": "ccf8144449c6d1441323a6efaacce806",
+    "url": "/blog/static/media/OldTown.ccf81444.jpg"
+  },
+  {
+    "revision": "ebb7a91fd9d7174c132c9e4b08b48c10",
+    "url": "/blog/static/media/Shap.ebb7a91f.PNG"
+  },
+  {
+    "revision": "1c93bb66698d0571e37ffad94267bb8e",
+    "url": "/blog/static/media/ViewFromCabotTower.1c93bb66.jpg"
+  },
+  {
+    "revision": "56733db5584b0e4e8e082495741a602d",
+    "url": "/blog/static/media/blogger.56733db5.PNG"
+  },
+  {
+    "revision": "e357505dd64a310c2e952c098d28f54a",
+    "url": "/blog/static/media/gull.e357505d.jpg"
+  },
+  {
+    "revision": "27486e857c102ecd168b9a54073325ad",
+    "url": "/blog/static/media/icomoon.27486e85.ttf"
+  },
+  {
+    "revision": "2a0cc8971f9c87819a1e76ca53a9029c",
+    "url": "/blog/static/media/icomoon.2a0cc897.svg"
+  },
+  {
+    "revision": "b685eba466302ea5642205c19a9e698c",
+    "url": "/blog/static/media/icomoon.b685eba4.woff"
+  },
+  {
+    "revision": "bba726b671c04eb61e2f7d5ced36c1a9",
+    "url": "/blog/static/media/icomoon.bba726b6.eot"
+  },
+  {
+    "revision": "69e1e992b935795dfc6a0aa1b8fe1f19",
+    "url": "/blog/static/media/pdp-min.69e1e992.PNG"
+  },
+  {
+    "revision": "cbf3350d737fd7b46901e47a38c6bc52",
+    "url": "/blog/static/media/star_list.cbf3350d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blog/precache-manifest.0f4381e0214cd110e4dc450b53a23059.js"
+  "/blog/precache-manifest.b862c4c482ec8bd9132888cd26fc8486.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/blog/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blog/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
