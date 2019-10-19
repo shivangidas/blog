4faf39bf3d292617799b291c732ed7288fd9092c
Shivangi <shivangi.das7@gmail.com>
2019-10-19 15:31:51 +0100

    Updates

---
+++ precache-manifest.3486e42ea60f4e6cb48462d4dfd774b6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "981a1f63df0ddce96ea5be07c811a0b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c9ecf75f9e4454fd44a",
+    "url": "/static/css/2.ca025b70.chunk.css"
+  },
+  {
+    "revision": "f2d3953d7cbda57ff72c",
+    "url": "/static/css/main.b5729b82.chunk.css"
+  },
+  {
+    "revision": "4c9ecf75f9e4454fd44a",
+    "url": "/static/js/2.56d96fc5.chunk.js"
+  },
+  {
+    "revision": "f2d3953d7cbda57ff72c",
+    "url": "/static/js/main.e1c5483e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "77e54c15db377272ca0743e9a0eae3d5",
+    "url": "/static/media/ChristmasSteps.77e54c15.jpg"
+  },
+  {
+    "revision": "ccf8144449c6d1441323a6efaacce806",
+    "url": "/static/media/OldTown.ccf81444.jpg"
+  },
+  {
+    "revision": "ebb7a91fd9d7174c132c9e4b08b48c10",
+    "url": "/static/media/Shap.ebb7a91f.PNG"
+  },
+  {
+    "revision": "1c93bb66698d0571e37ffad94267bb8e",
+    "url": "/static/media/ViewFromCabotTower.1c93bb66.jpg"
+  },
+  {
+    "revision": "56733db5584b0e4e8e082495741a602d",
+    "url": "/static/media/blogger.56733db5.PNG"
+  },
+  {
+    "revision": "e357505dd64a310c2e952c098d28f54a",
+    "url": "/static/media/gull.e357505d.jpg"
+  },
+  {
+    "revision": "27486e857c102ecd168b9a54073325ad",
+    "url": "/static/media/icomoon.27486e85.ttf"
+  },
+  {
+    "revision": "2a0cc8971f9c87819a1e76ca53a9029c",
+    "url": "/static/media/icomoon.2a0cc897.svg"
+  },
+  {
+    "revision": "b685eba466302ea5642205c19a9e698c",
+    "url": "/static/media/icomoon.b685eba4.woff"
+  },
+  {
+    "revision": "bba726b671c04eb61e2f7d5ced36c1a9",
+    "url": "/static/media/icomoon.bba726b6.eot"
+  },
+  {
+    "revision": "69e1e992b935795dfc6a0aa1b8fe1f19",
+    "url": "/static/media/pdp-min.69e1e992.PNG"
+  },
+  {
+    "revision": "cbf3350d737fd7b46901e47a38c6bc52",
+    "url": "/static/media/star_list.cbf3350d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blog/precache-manifest.5eef900ccd4dd2546484af15337c78f4.js"
+  "/precache-manifest.3486e42ea60f4e6cb48462d4dfd774b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blog/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
