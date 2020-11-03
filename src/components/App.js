85aa404351065e258536d635a89596498268b842
Shivangi <shivangi.das7@gmail.com>
2020-11-03 16:20:55 +0000

    the footer is finally fixed

--- src/components/App.js
+++ src/components/App.js
@@ -22,29 +22,32 @@ class App extends Component {
   render() {
     return (
       <HashRouter>
-        <div className="wrapper">
-          <main>
-            <div id="not-snackbar">Some text here</div>
-            <Header />
-            <Route exact path={["/", "/blog"]} component={Links} />
-            <Route path="/ReactAndMe" component={Post1} />
-            <Route path="/ServiceWorkers" component={Post2} />
-            <Route path="/Overfitted" component={Post3} />
-            <Route path="/Python" component={Post4} />
-            <Route path="/DeepCloneJS" component={Post5} />
-            <Route path="/ScholarshipStory" component={Post6} />
-            <Route path="/PermutationImportance" component={Post7} />
-            <Route path="/PartialPlots" component={Post8} />
-            <Route path="/ShapValues" component={Post9} />
-            <Route path="/LifeAtTimeZero" component={Post10} />
-            <Route path="/HowMean" component={Post11} />
-            <Route path="/Hooked" component={Post12} />
-            <Route path="/TravellingCat" component={Post13} />
-            <Route path="/Overstory" component={Post14} />
-            <Route path="/GoodOmens" component={Post15} />
-          </main>
+        <>
+          <div className="wrapper">
+            <main>
+              <div id="not-snackbar">Some text here</div>
+              <Header />
+              <Route exact path={["/", "/blog"]} component={Links} />
+              <Route path="/ReactAndMe" component={Post1} />
+              <Route path="/ServiceWorkers" component={Post2} />
+              <Route path="/Overfitted" component={Post3} />
+              <Route path="/Python" component={Post4} />
+              <Route path="/DeepCloneJS" component={Post5} />
+              <Route path="/ScholarshipStory" component={Post6} />
+              <Route path="/PermutationImportance" component={Post7} />
+              <Route path="/PartialPlots" component={Post8} />
+              <Route path="/ShapValues" component={Post9} />
+              <Route path="/LifeAtTimeZero" component={Post10} />
+              <Route path="/HowMean" component={Post11} />
+              <Route path="/Hooked" component={Post12} />
+              <Route path="/TravellingCat" component={Post13} />
+              <Route path="/Overstory" component={Post14} />
+              <Route path="/GoodOmens" component={Post15} />
+            </main>
+            <div className="push"></div>
+          </div>
           <Footer />
-        </div>
+        </>
       </HashRouter>
     );
   }
